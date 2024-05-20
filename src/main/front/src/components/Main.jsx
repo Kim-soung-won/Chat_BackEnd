66464734bffe9db12af97b70d99b68e752fe9bd9
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-05-20 20:50:51 +0900

    add React

---
+++ src/main/front/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React,  {useEffect, createContext, useReducer, useMemo} from "react";
+import axios from "axios";
+
+const Main = () => {
+    return (
+        <>
+            <h1>반갑다 나는 리엑트 머신이다.</h1>
+        </>
+    )
+}
+
+export default Main;
